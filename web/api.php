4b232a91cd61d0f0d75334ca6eb405cc9baf5a5d
Matt Langlois <fletchto99@gmail.com>
2015-04-21 21:20:00 -0400

    Cleanup

--- web/api.php
+++ web/api.php
@@ -1,21 +1,16 @@
 <?php
-
 require_once('configuration.php');
 require_once('functions/functions.php');
-
 $params = null;
-
 if (count($_POST) == 0) {
     $params = json_decode(file_get_contents('php://input'), true);
 } else {
     $params = $_POST;
 }
-
 if ($params !== null) {
     $functions = new functions(Configuration::getConfiguration(), $params['username'], $params['password']);
     $functions->execute($params['method']);
 } else {
     echo json_encode(array('error' => 'Server side error, please try again later.'));
 }
-
 ?>
