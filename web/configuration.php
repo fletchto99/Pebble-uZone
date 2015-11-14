995f288e0abce5ddd54e70b2e5d81c2ed4043df2
Matt Langlois <fletchto99@gmail.com>
2015-11-14 00:32:35 -0500

    Update to reflect the changes on the website

--- web/configuration.php
+++ web/configuration.php
@@ -1,15 +1,15 @@
 <?php
 
-class Configuration
-{
+class Configuration {
 
-    static function getConfiguration()
-    {
+    static function getConfiguration() {
         $config['LOGIN_URL'] = 'https://carteuottawacard.uottawa.ca/login.aspx?option=0&LanguagePreference=0';
         $config['ACCOUNT_URL'] = 'https://carteuottawacard.uottawa.ca/login.aspx?option=0&LanguagePreference=0';
         $config['BALANCE_URL'] = 'https://carteuottawacard.uottawa.ca/login.aspx?option=0&LanguagePreference=0';
-        $config['HISTORY_URL'] = 'https://carteuottawacard.uottawa.ca/login.aspx?option=0&LanguagePreference=0&ReturnUrl=%2facc_history.aspx';
+        $config['FLEX_HISTORY_URL'] = 'https://carteuottawacard.uottawa.ca/login.aspx?option=0&LanguagePreference=0&ReturnUrl=%2facc_history.aspx%3Facc%3D1';
+        $config['MEAL_HISTORY_URL'] = 'https://carteuottawacard.uottawa.ca/login.aspx?option=0&LanguagePreference=0&ReturnUrl=%2facc_history.aspx%3Facc%3D2';
         $config['ID'] = 'aspnetForm';
+
         return $config;
     }
 }
--- web/functions/functions.php
+++ web/functions/functions.php
@@ -9,30 +9,27 @@ require_once('MealHistory.php');
 require_once('FlexHistory.php');
 require_once('MealPlanCheck.php');
 
-class Functions
-{
+class Functions {
 
     private $username = null;
     private $password = null;
-    private $config = array();
+    private $config = [];
 
-    private $result = array('error' => 'Username or password not configured correctly.');
+    private $result = ['error' => 'Username or password not configured correctly.'];
 
-    function __construct($config, $username, $password)
-    {
+    function __construct($config, $username, $password) {
         $this->config = $config;
         $this->username = $username;
         $this->password = $password;
     }
 
-    function execute($method)
-    {
+    function execute($method) {
         if ($this->username && $this->password) {
             switch ($method) {
                 case 'CheckLogin':
-                    $form = array('ID' => $this->config['ID'], 'name' => $this->username, 'pass' => $this->password, 'optStudent' => 'optStudent');
+                    $form = ['ID' => $this->config['ID'], 'name' => $this->username, 'pass' => $this->password, 'optStudent' => 'optStudent'];
                     $login = self::checkLogin($form, $this->config['LOGIN_URL']);
-                    $this->result = array('login' => ($login ? 'success' : 'failed'));
+                    $this->result = ['login' => ($login ? 'success' : 'failed')];
                     break;
                 case 'MealPlanCheck':
                     $balance = new MealPlanCheck($this->username, $this->password, $this->config['ID'], $this->config['LOGIN_URL']);
@@ -43,11 +40,11 @@ class Functions
                     $this->result = $balance->execute();
                     break;
                 case 'MealHistory':
-                    $meal = new MealHistory($this->username, $this->password, $this->config['ID'], $this->config['HISTORY_URL']);
+                    $meal = new MealHistory($this->username, $this->password, $this->config['ID'], $this->config['MEAL_HISTORY_URL']);
                     $this->result = $meal->execute();
                     break;
                 case 'FlexHistory':
-                    $flex = new FlexHistory($this->username, $this->password, $this->config['ID'], $this->config['HISTORY_URL']);
+                    $flex = new FlexHistory($this->username, $this->password, $this->config['ID'], $this->config['FLEX_HISTORY_URL']);
                     $this->result = $flex->execute();
                     break;
                 case 'Deactivate':
@@ -62,39 +59,38 @@ class Functions
         echo json_encode($this->result);
     }
 
-    static function findElement($html, $class, $pos)
-    {
+    static function findElement($html, $class, $pos) {
         $DOM = new simple_html_dom();
         $DOM->load($html);
         if ($DOM->find($class)) {
             return array_values($DOM->find($class))[$pos];
         }
+
         return false;
     }
 
-    static function elementToPlaintext($DOMelement)
-    {
+    static function elementToPlaintext($DOMelement) {
         return $DOMelement->plaintext;
     }
 
-    static function findElements($html, $class)
-    {
+    static function findElements($html, $class) {
         $DOM = new simple_html_dom();
         $DOM->load($html);
         if ($DOM->find($class)) {
             return $DOM->find($class);
         }
+
         return null;
     }
 
-    static function checkLogin($form_fill, $url)
-    {
+    static function checkLogin($form_fill, $url) {
         $scrape = new uCardScrape($url, $form_fill);
         $html = $scrape->fetchResult();
 
         if (strpos($html, 'Login failed for Student number') !== false) {
             return '';
         }
+
         return $html;
     }
 
